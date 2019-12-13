5c8696e4088ea6e5b9e34e950753e63d63ff31bb
JACKSUYON <57020189+jacksuyon@users.noreply.github.com>
2019-12-13 10:50:28 -0500

    SE AGREGO LOS 15 EJERCICIOS DE LA OPERACION COMPARACION

---
+++ JACK/3_comparacion.py
@@ -0,0 +1,85 @@
+#01 ejercicio
+cadena1 = "Junio"
+cadena2 = "Julio"
+
+if cadena1 == cadena2:
+    print ("Las cadenas de caracteres son iguales.")
+else:
+    print ("Las cadenas de caracteres no son iguales.")
+
+#02 ejercicio
+print("AAAAA"=="AAAAA")
+
+#03 ejercicio
+ganador_de_la_copa_del_mundo="Francia"
+mejor_inchada="Peru"
+print(ganador_de_la_copa_del_mundo==mejor_inchada)
+
+#04 ejercicio
+codigo_prueba="123C"
+codigo_examen="123C"
+if(codigo_prueba==codigo_examen):
+    print("Codigo correcto")
+else:
+    print("Codigo incorrecto")
+
+#05 ejercicio
+escritor = "Cesar Vallejo"
+escritor_de_TRILCE = escritor
+
+if escritor is escritor_de_TRILCE:
+    print ("Cesar Vallejo es el escritor de TRILCE")
+else:
+    print ("Cesar Vallejo no es el escritor de trilce")
+
+#06 ejercicio
+print("x" < "y" )     # x es menor que y
+
+#07 ejercicio
+a="5"
+b="6"
+if(a==b):
+    print("messi a ganado 5 balones de oro")
+else:
+    print("messi no a ganado 5 balones de oro")
+
+#08 ejercicio
+print("123"!="3434")
+
+#09 ejercicio
+usuario1="Juan"
+usuario2="Juan"
+if(usuario1==usuario2):
+    print("usuario correcto")
+else:
+    print("usuario incorrecto")
+
+#10 ejercicio
+print("HOLA JACK"=="hola jack")
+
+#11 ejercicio
+mejor_jugador_1="Messi"
+mejor_jugador_2="Cristiano Ronaldo"
+print(mejor_jugador_1==mejor_jugador_2)
+
+#12 ejercicio
+print("Suyon Riojas"=="Riojas Suyon")
+
+#13 ejercicio
+respuesta="D"
+respuesta_1="D"
+if(respuesta==respuesta_1):
+    print("respuesta correcta")
+else:
+    print("respuesta incorrecta")
+#14 ejercicio
+cad="Heraldos Negros"
+autor="Perros hambrientos"
+if(cad==autor):
+    print("obras de Cesar vallejo")
+else:
+    print("No son Obras del escritor Cesar Vallejo ")
+
+#15 ejercicio
+
+print("12"<"13"<"14")
