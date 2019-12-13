b7f90202a4450520c722b267a332d9376003d026
JACKSUYON <57020189+jacksuyon@users.noreply.github.com>
2019-12-13 10:52:16 -0500

    SE AGREGO LOS 15 EJERCICIOS DE LA OPERACION ITERACION

---
+++ JACK/6_iteracion.py
@@ -0,0 +1,107 @@
+#ejercicio01
+mensaje="HOLA AMIGOS DE LA UNIVERSIDAD"
+for letra in mensaje:
+    print(letra)
+
+#ejercicio02
+msg="ME GUSTAS CUANDO CALLAS PORQUE ESTAS COMO AUSENTE"
+contador_A=0
+for letra in msg:
+    if(letra=="A"):
+        contador_A +=1
+print("A:",contador_A)
+
+#ejercicio03
+msg="HOY LA TIERRA Y EL CIELO ME SONRIEN"
+contador_R=0
+for letra in msg:
+    if(letra=="R"):
+        contador_R +=1
+print("R:",contador_R)
+
+#ejercicio04
+msg="AL PERDERTE YO A TI, TU Y YO HEMOS PERDIDO"
+contador_Y=0
+for letra in msg:
+    if(letra=="Y"):
+        contador_Y +=1
+print("Y:",contador_Y)
+
+#ejercicio05
+msg="JUVENTUD DIVINO TESORO YA TE VAS PARA NO VOLVER"
+contador_O=0
+for letra in msg:
+    if(letra=="O"):
+        contador_O +=1
+print("O:",contador_O)
+
+#ejercicio06
+msg="CUANDO QUIERO LLORAR NO LLORO, Y A VECES LLORO SIN QUERER"
+contador_E=0
+for letra in msg:
+    if(letra=="E"):
+        contador_R +=1
+print("E:",contador_E)
+
+#ejercicio07
+msg="DICHOSO EL ARBOL QUE ES APENAS SENSITIVO"
+contador_S=0
+for letra in msg:
+    if(letra=="S"):
+        contador_S +=1
+print("S:",contador_S)
+
+#ejercicio08
+msg="CAMINANTES SON TUS HUELLAS EL CAMINO Y NADA MAS"
+contador_M=0
+for letra in msg:
+    if(letra=="M"):
+        contador_M +=1
+print("M:",contador_M)
+
+#ejercicio09
+msg="HOMBRES QUE PARECEN SOMBRA"
+contador_B=0
+for letra in msg:
+    if(letra=="B"):
+        contador_B +=1
+print("B:",contador_B)
+
+#ejercicio10
+msg="COMENTARIO REALES DE LOS INCAS"
+contador_C=0
+for letra in msg:
+    if(letra=="C"):
+        contador_C +=1
+print("C:",contador_C)
+
+#ejercicio11
+msg="EL AMOR ES ESA PISTOLA, CON LA QUE TODOS NOS SUICIDAMOS UNA VES"
+contador_P=0
+for letra in msg:
+    if(letra=="P"):
+        contador_P +=1
+print("P:",contador_P)
+
+#ejercicio12
+msg="AMIGOS SINCEROS LOS CUENTO CON UNA SOLA MANO"
+contador_U=0
+for letra in msg:
+    if(letra=="U"):
+        contador_U +=1
+print("U:",contador_U)
+
+#ejercicio13
+msg="LA VIDA ES UN VIAJE NO UNA ESTACION"
+contador_I=0
+for letra in msg:
+    if(letra=="I"):
+        contador_I +=1
+print("I:",contador_I)
+
+#ejercicio14
+
+#ejercicio15
+mensaje="Programacion"
+for letra in mensaje:
+    print(letra)
