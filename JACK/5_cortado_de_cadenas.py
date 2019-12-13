da84ba6c6b477a92c6558b05eec43cba30ec1152
JACKSUYON <57020189+jacksuyon@users.noreply.github.com>
2019-12-13 10:51:37 -0500

    SE AGREGO LOS 15 EJERCICIOS DE LA OPERACION CORTADO DE CADENA

---
+++ JACK/5_cortado_de_cadenas.py
@@ -0,0 +1,164 @@
+#ejercicio01
+#              1         2         3         4         5         6         7         8         9
+#    01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567
+msg="RECUERDE EL ALMA ADORMIDA, AVIVE EL SESO Y DESPIERTE ,COMO SE PASA LA VIDA,COMO SE VIENE LA MUERTE"
+#AVIVE
+print(msg[27:32])
+#LA VIDA
+print(msg[67:74])
+#MUERTE
+print(msg[92:])
+
+#ejercicio02
+#              1         2         3         4         5         6
+#    01234567890123456789012345678901234567890123456789012345678901234
+msg="ODROS OELO NU NE ARAC IM AV ES ,ATOG A ATOG OPREUC IM IT ED AV ES"
+#GOTA
+print(msg[35:31:-1])
+#MI CARA
+print(msg[23:16:-1])
+#CUERPO
+print(msg[49:43:-1])
+
+#ejercicio03
+#              1         2         3         4         5         6         7         8         9
+#    01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234
+msg="PUES SEÑOR GOBERNADOR, MIREO BIEN POR ENTERO QUE ALLA VA EL RECOGEDOR Y AQUI QUEDA EL CARNICERO"
+#GOBERNADOR
+print(msg[11:21])
+#EL RECOGEDOR
+print(msg[57:69])
+#ADEUQ
+print(msg[81:76:-1])
+
+#ejercicio04
+#              1         2
+#    012345678901234567890123456789
+msg="COMENTARIO REALES DE LOS INCAS"
+#REALES
+print(msg[11:17])
+#INCAS
+print(msg[25:])
+#REAL
+print(msg[11:15])
+
+#ejercicio05
+#              1         2         3         4         5         6         7
+#    01234567890123456789012345678901234567890123456789012345678901234567890123
+msg="EL PERU ES UN ORGANISMO ENFERMO DONDE, DONDE SE PONE EL DEDO, SALTA LA PUS"
+#DON
+print(msg[32:35])
+#EL DEDO
+print(msg[53:60])
+#ENFERMO
+print(msg[24:31])
+
+#ejercicio06
+#              1         2
+#    01234567890123456789012345
+msg="HOMBRES QUE PARECEN SOMBRA"
+#PARECEN
+print(msg[12:19])
+#SER
+print(msg[6:3:-1])
+#QUE
+print(msg[8:11])
+
+#ejercicio07
+#              1         2         3         4
+#    01234567890123456789012345678901234567890123456
+msg="CAMINANTES SON TUS HUELLAS EL CAMINO Y NADA MAS"
+#MINO
+print(msg[32:36])
+#NADA
+print(msg[39:43])
+#CAMINANTES
+print(msg[:10])
+
+#ejercicio08
+#              1         2         3
+#    012345678901234567890123456789012345678
+msg="DICHOSO EL ARBOL QUE ES APENAS SENSITIVO"
+#ES APENAS
+print(msg[21:30])
+#EL ARBOL
+print(msg[8:16])
+#SENSITIVO
+print(msg[31:])
+
+#ejercicio09
+#              1         2         3         4         5
+#    012345678901234567890123456789012345678901234567890123456
+msg="CUANDO QUIERO LLORAR NO LLORO, Y A VECES LLORO SIN QUERER"
+#NO LLORO
+print(msg[21:29])
+#SIN QUERER
+print(msg[47:])
+#0DNAUC
+print(msg[5::-1])
+
+#ejercicio10
+#              1         2         3
+#    01234567890123456789012345678901234
+msg="SAJEUQ SIM A LOMRAM EUQ ARUD SAM HO"
+#MARMOL
+print(msg[18:12:-1])
+#MIS
+print(msg[9:6:-1])
+#MAR
+print(msg[18:15:-1])
+
+#ejercicio11
+#              1         2         3         4
+#    01234567890123456789012345678901234567890123456
+msg="JUVENTUD DIVINO TESORO YA TE VAS PARA NO VOLVER"
+#VOLVER
+print(msg[41:])
+#TESORO
+print(msg[16:22])
+#VINO
+print(msg[11:15])
+
+#ejercicio12
+#              1         2         3         4
+#    012345678901234567890123456789012345678901
+msg="AL PERDERTE YO A TI, TU Y YO HEMOS PERDIDO"
+#TU Y YO
+print(msg[21:28])
+#AL PERDERTE YO A TI, TU Y YO HEMOS PERDIDO
+print(msg[:])
+#PERDER
+print(msg[3:9])
+
+#ejercicio13
+#              1         2         3
+#    01234567890123456789012345678901234
+msg="HOY LA TIERRA Y EL CIELO ME SONRIEN"
+#TIERRA
+print(msg[7:13])
+#CIELO
+print(msg[19:24])
+#EM
+print(msg[26:24:-1])
+
+#ejercicio14
+#              1         2         3         4
+#    0123456789012345678901234567890123456789012345678
+msg="ME GUSTAS CUANDO CALLAS PORQUE ESTAS COMO AUSENTE"
+#ESTAS
+print(msg[31:36])
+#OMOC
+print(msg[40:36:-1])
+#GUSTAS
+print(msg[3:9])
+
+#ejercicio15
+#              1         2         3
+#    0123456789012345678901234567890123
+msg="ODAGEP ZIRAN ANU A ERBMOH NU ESARE"
+#PEGA
+print(msg[5:1:-1])
+#A UNA
+print(msg[17:12:-1])
+#UN HOMBRE
+print(msg[27:18:-1])
