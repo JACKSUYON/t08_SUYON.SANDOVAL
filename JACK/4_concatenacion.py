0600df5f9602aada7c442c28c8c6b2299f8dfffd
JACKSUYON <57020189+jacksuyon@users.noreply.github.com>
2019-12-13 10:51:07 -0500

    SE AGREGO LOS 15 EJERCICIOS DE LA OPERACION CONCATENACION

---
+++ JACK/4_concatenacion.py
@@ -0,0 +1,93 @@
+#concatenacion nro1
+nombre="Percy"
+producto="yogur"
+tienda="Milagritos"
+lugar="tucume"
+print(nombre+" compra un "+producto+" en la tienda " +"\""+tienda+"\"" +" que queda en"+lugar)
+
+#concatenacion nro2
+motivo="aniversario"
+lugar="Piura"
+universitario="Luis"
+uni="UNPRG"
+print(universitario+" ira de paseo a "+lugar+" por el "+motivo+" de la "+uni)
+
+#concatenacion nro3
+madre="Marleny"
+padre="Esteban"
+puesto="1er puesto"
+grado="sexto"
+nivel="primaria"
+print(madre+" y "+padre+" son los padres de Angie quien cursa el "+grado+ " grado"+" de "+nivel+" ocupando el "+puesto+"en su salon")
+
+#concatenacion nro4
+descubridor="Cristobal Colon"
+continente="America"
+tierra="redonda"
+print(descubridor+" es el descubridor del continente "+continente+" y dijo que la tierra era "+tierra)
+
+#concatenacion nro5
+empresa="Transcom"
+señor="Jose"
+rendimiento="excelente"
+print("La empresa "+empresa+" es muy buena, alli trabaja el señor "+señor+" cuyo rendimiento es "+rendimiento)
+
+#concatenacion nro6
+nombre="jose carlos"
+escuela="ingenieria electronica"
+curso="programacion"
+print(nombre+" estudia "+"\""+escuela+"\""+" y lleva el curso"+" \""+curso+"\"")
+
+#concatenacion nro7
+cantante="Tyron Gonzales"
+genero="Rap"
+año="2015"
+conocido="CANCERBERO"
+print("el cantante "+cantante+" es uno de los mas grandes cantantes del "+genero+" mas conocido como " + "\""+conocido+"\""+" lamentablemente murio en el año "+año)
+
+#concatenacion nro8
+jugador="Messi"
+club="Barcelona"
+premio="5 balones de oro"
+rival="Cristiano Ronaldo"
+print("el mejor jugador de mundo es "+jugador+" que a ganado "+premio+" con el club "+club+" y su su rival es "+rival+" tambien uno de los mejores jugadores del mundo")
+
+#concatenacion nro9
+Cantante="Shaw Mendes"
+Enamorada="Camila Cabello"
+Edad="19 años"
+Genero="Pop"
+print("el famoso cantante "+Cantante+" tiene "+Edad+" pertenece al genero "+Genero+" y su enamorada es "+Enamorada)
+
+#concatenacion nro10
+Curso="Programacion"
+programa="Python"
+tema="Operaciones"
+print("el curso de "+Curso+" trabajamos con el programa "+programa+" y estamos en el tema "+tema)
+
+#concatenacion nro11
+antivirus="Bitdefender Antivirus Plus"
+funcion="proteger , mantener  limpio  y en buen estado nuestra PC"
+print("el antivirus "+antivirus+ "es un programa cuya funcion "+funcion)
+
+#concatenacion nro12
+restaurante="Las Balzas"
+sitio="Tucume"
+referencia="Al frente del complejo deportivo"
+print("el restaurante "+restaurante+" es uno de los mejores "+", y queda en "+sitio+" "+referencia)
+#concatenacion nro13
+region="Lambayeque"
+universidad="UNPRG"
+print("la universidad "+universidad+" es la mejor en la region "+region)
+
+#concatenacion nro14
+examen="Examen de admision 2020"
+modalidad="Ordinario"
+grupo="I"
+print("julio dara el "+examen+" bajo la modalidad de "+modalidad+" del grupo "+grupo)
+
+#concatenacion nro15
+concurso="belleza"
+estudios="modelaje"
+sueño="Mis Peru"
+print("Rosa es una señorita que a ganado un concurso de "+concurso+" en su colegio "+"y piensa estudiar "+estudios+", su gran sueño es ser "+sueño)
