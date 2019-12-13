853e60969da902a1b81c97bb40fe889bd786c596
JACKSUYON <57020189+jacksuyon@users.noreply.github.com>
2019-12-13 10:48:13 -0500

    SE AGREGO LOS 15 EJERCICIOS DE LA OPERACION INDEXACION DEL ESTUDIANTE JACK

---
+++ JACK/1_indexacion.py
@@ -0,0 +1,48 @@
+#                 1         2         3         4
+#       01234567890123456789012345678901234567890123
+cadena="EL HOMBRE NACE LIBRE LA SOCIEDAD LO CORROMPE"
+
+#manipulador de texto nro 1
+print(cadena[12],cadena[11],cadena[24],cadena[22])  #me imprime los caracteres  " C A S A "
+
+#manipulador de texto nro 2
+print(cadena[3],cadena[4],cadena[1],cadena[11])  #me imprime los caracteres  " H O L A "
+
+#manipulador de texto nro 3
+print(cadena[12],cadena[22],cadena[38],cadena[39],cadena[34])  #me imprime los caracteres  " C A R R O "
+
+#manipulador de texto nro 4
+print(cadena[24],cadena[11],cadena[38],cadena[30])  #me imprime los caracteres  " S A R A "
+
+#manipulador de texto nro 5
+print(cadena[15],cadena[27],cadena[41],cadena[22])  #me imprime los caracteres  " L I M A "
+
+#manipulador de texto nro 6
+print(cadena[15],cadena[40],cadena[39],cadena[37])  #me imprime los caracteres  " L O R O "
+
+#manipulador de texto nro 7
+print(cadena[42],cadena[43],cadena[38],cadena[39],cadena[40])  #me imprime los caracteres  " P E R R O "
+
+#manipulador de texto nro 8
+print(cadena[36],cadena[37],cadena[42],cadena[11])  #me imprime los caracteres  " C O P A "
+
+#manipulador de texto nro 9
+print(cadena[34],cadena[24],cadena[25])  #me imprime los caracteres  " O S O"
+
+#manipulador de texto nro 10
+print(cadena[29],cadena[16],cadena[34],cadena[24])  #me imprime los caracteres  " D I O S "
+
+#manipulador de texto nro 11
+print(cadena[22],cadena[41],cadena[25],cadena[38])  #me imprime los caracteres  " A M O R "
+
+#manipulador de texto nro 12
+print(cadena[24],cadena[25],cadena[42],cadena[11])  #me imprime los caracteres  " S O P A "
+
+#manipulador de texto nro 13
+print(cadena[42],cadena[43],cadena[33],cadena[34])  #me imprime los caracteres  " P E L O "
+
+#manipulador de texto nro 14
+print(cadena[24],cadena[22],cadena[42],cadena[25])  #me imprime los caracteres  " S A P O "
+
+#manipulador de texto nro 15
+print(cadena[24],cadena[25],cadena[10],cadena[16],cadena[29],cadena[34])  #me imprime los caracteres  " S O N I D O "
