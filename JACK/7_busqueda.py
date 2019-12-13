489ad81739e16c155fc36eb48925f4fec08abd75
JACKSUYON <57020189+jacksuyon@users.noreply.github.com>
2019-12-13 10:52:49 -0500

    SE AGREGO LOS 15 EJERCICIOS DE LA OPERACION BUSQUEDA

---
+++ JACK/7_busqueda.py
@@ -0,0 +1,101 @@
+#ejercicio01
+#                     1         2         3
+#           01234567890123456789012345678901
+estudiante="JACK EDINSON SUYON RIOJAS"
+print(estudiante.find("SUYON"))
+
+#ejercicio02
+#                1         2         3         4
+#      01234567890123456789012345678901234567890
+frase="\"la vida es un viaje , no una estacion\""
+print(frase.index("una"))
+
+#ejercicio03
+#                 1
+#       012345678901234567
+curso="\"Programacion\""
+print(curso.find("g"))
+
+#ejercicio04
+#                    1         2
+#          01234567890123456789012
+carrera="\"Ingenieria Electronica\""
+print(carrera.find("l"))
+
+
+#ejercicio05
+#              1         2         3         4
+#         0123456789012345678901234567890123456789012345
+facultad="\"Facultad de Ciencias Fisicas y Matematicas\""
+print(facultad.find("Matematicas"))
+
+
+#ejercicio06
+#              1         2
+#        0123456789012345678901234
+colegio="EXCELENCIA COLLEGE TUCUME"
+print(colegio.find("T"))
+
+
+#ejercicio07
+#              1         2         3         4
+#          0123456789012345678901234567890123456
+promocion="JOVENES EMPRENDEDORES SUPERANDO METAS"
+print(promocion.find("SU"))
+
+
+#ejercicio08
+#                      1         2
+#            01234567890123456789012
+institucion="Bienestar Universitario"
+print(institucion.find("U"))
+
+
+#ejercicio09
+#                 1         2         3         4         5         6         7         8         9         10
+#       0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012
+poesia="Debes amar sin miedo a ser traicionado, aunque sin darle prioridad al que prioridad no te ha dado claro"
+print(poesia.find("prioridad"))
+
+
+#ejercicio10
+#                  1
+#        01234567890123456
+cancion="lamento boliviano"
+print(cancion.find("v"))
+
+
+#ejercicio11
+#                     1         2         3         4         5
+#           01234567890123456789012345678901234567890123456789012
+adivinanza="oro no es plata no es habre la cortina y veras que es"
+print(adivinanza.find("cortina"))
+
+
+#ejercicio12
+#                      1         2
+#            01234567890123456789012
+restaurante="EL BUEN SABOR DEL NORTE"
+print(restaurante.find("S"))
+
+
+#ejercicio13
+#                    1
+#          0123456789012345678
+panaderia="PANADERIA TODO RICO"
+print(panaderia.find("T"))
+
+
+#ejercicio14
+
+#        01234567
+empresa="TRANSCOM"
+print(empresa.find("O"))
+
+
+#ejercicio15
+#                 1
+#       012345678901234567
+cuento="La Caperucita Roja"
+print(cuento.find("Ro"))
+
