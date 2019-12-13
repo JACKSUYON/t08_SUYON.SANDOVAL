17d5f84a04ab38109cce4978ab981989a7146854
JACKSUYON <57020189+jacksuyon@users.noreply.github.com>
2019-12-13 10:49:45 -0500

    SE AGREGO LOS 15 EJERCICIOS DE LA OPERACION LONGITUD

---
+++ JACK/2_longitud.py
@@ -0,0 +1,74 @@
+#ejercicio01
+estudiante="JACK EDINSON SUYON RIOJAS"
+print(estudiante)
+print("1_len:",len(estudiante)) #Imprime la longitud de la cadena estudiante
+
+#ejercicio02
+frase="\"la vida es un viaje , no una estacion\""
+print(frase)
+print("2_len:",len(frase)) #Imprime la longitud de la cadena frase
+
+#ejercicio03
+curso="\"Programacion\""
+print(curso)
+print("3_len:",len(curso)) #Imprime la longitud de la cadena curso
+
+#ejercicio04
+carrera="\"Ingenieria Electronica\""
+print(carrera)
+print("4_len:",len(carrera)) #Imprime la longitud de la cadena carrera
+
+#ejercicio05
+facultad="\"Facultad de Ciencias Fisicas y Matematicas\""
+print(facultad)
+print("5_len:",len(facultad)) #Imprime la longitud de la cadena facultad
+
+#ejercicio06
+colegio="EXCELENCIA COLLEGE TUCUME"
+print(colegio)
+print("6_len:",len(colegio)) #Imprime la longitud de la cadena colegio
+
+#ejercicio07
+promocion="JOVENES EMPRENDEDORES SUPERANDO METAS"
+print(promocion)
+print("7_len:",len(promocion)) #Imprime la longitud de la cadena promocion
+
+#ejercicio08
+institucion="Bienestar Universitario"
+print(institucion)
+print("8_len:",len(institucion)) #Imprime la longitud de la cadena institucion
+
+#ejercicio09
+poesia="Debes amar sin miedo a ser traicionado, aunque sin darle prioridad al que prioridad no te ha dado claro"
+print(poesia)
+print("9_len:",len(poesia)) #Imprime la longitud de la cadena poesia
+
+#ejercicio10
+cancion="lamento boliviano"
+print(cancion)
+print("10_len:",len(cancion)) #Imprime la longitud de la cadena cancion
+
+#ejercicio11
+adivinanza="oro no es plata no es habre la cortina y veras que es"
+print(adivinanza)
+print("11_len:",len(adivinanza)) #Imprime la longitud de la cadena adivinanza
+
+#ejercicio12
+restaurante="EL BUEN SABOR DEL NORTE"
+print(restaurante)
+print("12_len:",len(restaurante)) #Imprime la longitud de la cadena restaurante
+
+#ejercicio13
+panaderia="PANADERIA TODO RICO"
+print(panaderia)
+print("13_len:",len(panaderia)) #Imprime la longitud de la cadena panaderia
+
+#ejercicio14
+empresa="TRANSCOM"
+print(empresa)
+print("14_len:",len(empresa)) #Imprime la longitud de la cadena empresa
+
+#ejercicio15
+cuento="La Caperucita Roja"
+print(cuento)
+print("15_len:",len(cuento)) #Imprime la longitud de la cadena cuento
